53fb654072f0aeb18c8526545ada8d626c34aa0b
Diego Obando <diegoobando20@gmail.com>
2021-01-27 02:54:22 -0300

    mining

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # Blockchain.rs
 
 Blockchain implementation in Rust :crab:
+
+## Block Hashing:
+
+1. Concatena juntos todos los bytes que componen un bloque
+2. Genera una huella digital única del bloque (el hash) 
+
+## Nonce
+
+Un hash es único, como una huella digital de cierta data. Por lo tanto, para que un bloque sea valido, debemos de alguna manera cambiar los bytes que enviamos a la función. Recuerda que tan solo un pequeño cambio en el input cambia el hash resultante drásticamente. Este efecto es llamado *avalanching*.
+
+Por supuesto, no podemos cambiar la información almacenada en un bloque. Por lo tanto, introducimos un pieza de data adicional llamada **nonce**: un arbitrario(aunque no necesariamente aleatorio) valor añadido como campo a cada bloque, y hasheado junto con la data. Ya que ha sido declarado arbitrariamente, podemos cambiarlo como nos parezca conveniente.
+
+Puedes pensar en esto como: generar el hash correcto para un bloque es como un puzzle, y el nonce es la llave para ese puzzle. The proceso de intentar encontrar la llave es llamado **mining**.
+
+## Mining Strategy (Algorithm)
+
+1. Generar un nuevo nonce
+2. Hash bytes(esto es un paso computacionalmente pesado)
+3. Check hash against difficulty
+   a. Insifficient? Go back to step 1
+   b. Sufficient? Continue to step 2
+4. Añade el bloque a la cadena 
--- src/block.rs
+++ src/block.rs
@@ -8,15 +8,17 @@ pub struct Block {
     pub pre_block_hash: BlockHash,
     pub nonce: u64,
     pub payload: String,
+    pub difficulty: u128,
 }
 
 impl Debug for Block {
     fn fmt (&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "Block[{}]: {} at: {} width: {}", 
+        write!(f, "Block[{}]: {} at: {} width: {} nonce: {}", 
                     &self.index,
                     &hex::encode(&self.hash),
                     &self.timestamp,
-                    &self.payload)
+                    &self.payload,
+                    &self.nonce)
     }
 }
 
@@ -26,7 +28,8 @@ impl Block {
         timestamp : u128,
         pre_block_hash: BlockHash,
         nonce: u64,
-        payload: String
+        payload: String,
+        difficulty: u128
     ) -> Self {
         Block {
             index,
@@ -35,6 +38,18 @@ impl Block {
             pre_block_hash,
             nonce,
             payload,
+            difficulty,
+        }
+    }
+
+    pub fn mine(&mut self ) {
+        for nonce_attempt in 0..(u64::max_value()) {
+            self.nonce = nonce_attempt;
+            let hash = self.hash();
+            if check_difficulty(&hash, self.difficulty) {
+                self.hash = hash;
+                return;
+            }
         }
     }
 }
@@ -49,7 +64,11 @@ impl Hashable for Block {
         bytes.extend(&self.pre_block_hash);
         bytes.extend(&u64_bytes(&self.nonce));
         bytes.extend(self.payload.as_bytes());
-
+        bytes.extend(&u128_bytes(&self.difficulty));
         bytes
     }
+}
+
+pub fn check_difficulty (hash: &BlockHash, difficulty: u128) -> bool {
+    difficulty > difficulty_bytes_as_u128(&hash)
 }
