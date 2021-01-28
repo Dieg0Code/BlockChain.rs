7ffe39a8fe4620285f27fd32adeec9afa3825626
Diego Obando <diegoobando20@gmail.com>
2021-01-28 01:39:51 -0300

    Transactions part 2

--- src/block.rs
+++ src/block.rs
@@ -1,48 +1,50 @@
-use std::fmt::{ self, Debug, Formatter };
 use super::*;
+use std::fmt::{self, Debug, Formatter};
 
 pub struct Block {
     pub index: u32,
-    pub timestamp : u128,
-    pub hash: BlockHash,
-    pub prev_block_hash: BlockHash,
+    pub timestamp: u128,
+    pub hash: Hash,
+    pub prev_block_hash: Hash,
     pub nonce: u64,
-    pub payload: String,
+    pub transactions: Vec<Transaction>,
     pub difficulty: u128,
 }
 
 impl Debug for Block {
-    fn fmt (&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "Block[{}]: {} at: {} width: {} nonce: {}", 
-                    &self.index,
-                    &hex::encode(&self.hash),
-                    &self.timestamp,
-                    &self.payload,
-                    &self.nonce)
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(
+            f,
+            "Block[{}]: {} at: {} width: {} nonce: {}",
+            &self.index,
+            &hex::encode(&self.hash),
+            &self.timestamp,
+            &self.transactions.len(),
+            &self.nonce
+        )
     }
 }
 
 impl Block {
     pub fn new(
         index: u32,
-        timestamp : u128,
-        prev_block_hash: BlockHash,
-        nonce: u64,
-        payload: String,
-        difficulty: u128
+        timestamp: u128,
+        prev_block_hash: Hash,
+        transactions: Vec<Transaction>,
+        difficulty: u128,
     ) -> Self {
         Block {
             index,
             timestamp,
-            hash: vec![0; 32],// sha256 256 bits = 32 bytes
+            hash: vec![0; 32], // sha256 256 bits = 32 bytes
             prev_block_hash,
-            nonce,
-            payload,
+            nonce: 0,
+            transactions,
             difficulty,
         }
     }
 
-    pub fn mine(&mut self ) {
+    pub fn mine(&mut self) {
         for nonce_attempt in 0..(u64::max_value()) {
             self.nonce = nonce_attempt;
             let hash = self.hash();
@@ -63,12 +65,17 @@ impl Hashable for Block {
         bytes.extend(&u128_bytes(&self.timestamp));
         bytes.extend(&self.prev_block_hash);
         bytes.extend(&u64_bytes(&self.nonce));
-        bytes.extend(self.payload.as_bytes());
+        bytes.extend(
+            self.transactions
+                .iter()
+                .flat_map(|transaction| transaction.bytes())
+                .collect::<Vec<u8>>(),
+        );
         bytes.extend(&u128_bytes(&self.difficulty));
         bytes
     }
 }
 
-pub fn check_difficulty (hash: &BlockHash, difficulty: u128) -> bool {
+pub fn check_difficulty(hash: &Hash, difficulty: u128) -> bool {
     difficulty > difficulty_bytes_as_u128(&hash)
 }
